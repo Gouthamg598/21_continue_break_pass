0688c6014103c4c1345b1002b8d9acfbbd5e4e14
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-06-09 14:22:51 +0530

    Add files via upload

---
+++ 19_break_loop.py
@@ -0,0 +1,21 @@
+'''break== is used to print upto condition is true'''
+# n=-1
+# while(n<20):
+#     n+=2
+#     if n>8:
+#         break
+#     print(n)
+
+# n=0
+# while(n<10):
+#     n+=1
+#     if n==4:
+#         break
+#     print(n)
+
+# n=0
+# while(n<20):
+#     n+=2
+#     if(n==10):
+#         break
+#     print(n)
---
+++ 20_not_to_display_with_continue.py
@@ -0,0 +1,50 @@
+
+'''continue== which used to ignore/skip the printing upto given condition'''
+# n=0
+# while n<10:
+#     n+=1
+#     if n==6:
+#         continue
+#     print(n)
+
+
+""" 2nd type"""
+
+# n=0
+# while n<10:
+#     n+=1
+#     if n<5:
+#         continue
+#     print(n)
+
+
+""" 3rd type """
+
+# n=0
+# while n<10:
+#     n+=1
+#     if n>5:
+#         continue
+#     print(n)
+
+
+""" 4th type  """
+
+# n=0
+# while n<10:
+#     n+=1
+#     if n<=5:
+#         continue
+#     print(n)
+
+'''pss== is used to avoid printing in this case for true statement we wrutten pass nothing will exected for but exected for false '''
+# if (11>2):
+#     pass
+# else:
+#     print('False')
+
+
+# if (11<2):
+#     print('true')
+# else:   
+#     pass
